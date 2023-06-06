af1ddd0d15c6b3063ac75821b8a833b4f643d635
ismb99 <im_077@hotmail.com>
2023-06-06 17:59:27 +0200

    added new states

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,31 @@ import Quiz from "./Quiz";
 import data from "../data";
 
 export default function App() {
-  const [quizData, setQuizData] = useState(data.results);
+  const answers = data.results.map((answer) => {
+    return answer.correct_answer;
+  });
 
-  return <div>{<Quiz quiz={quizData} />}</div>;
+  const handleClick = (event) => {
+    const value = event.target.textContent;
+    let inputs = [...btnValue, value];
+
+    setBtnValue(inputs);
+
+    console.log("Iputs -> ", inputs);
+  };
+
+  const [quizData, setQuizData] = useState(data.results);
+  const [btnValue, setBtnValue] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState(answers);
+  // const [wrongtAnswers, setWrongAnswers] = useState([]);
+
+  // om btnValue == correctAnswer => 1 poäng
+  //
+
+  useEffect(() => {}, [btnValue]);
+  // console.log("btnValue -> ", btnValue);
+
+  // console.log("-> ", correctAnswers);
+
+  return <>{<Quiz quiz={quizData} handleClick={handleClick} />}</>;
 }
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,22 +1,29 @@
 import he from "he";
+import React, { useState, useEffect } from "react";
 
 export default function Quiz(props) {
   const quizElements = props.quiz.map((q, index) => {
     const decodedString = he.decode(q.question);
-    console.log(decodedString);
 
     return (
       <div key={`${index}-${q.question}`}>
         <h1>{decodedString}</h1>
-        <button>{q.correct_answer}</button>
-        <button>{q.incorrect_answers[0]}</button>
-        <button>{q.incorrect_answers[1]}</button>
-        <button>{q.incorrect_answers[2]}</button>
+        <button onClick={(event) => props.handleClick(event)}>
+          {q.correct_answer}
+        </button>
+        <button onClick={(event) => props.handleClick(event)}>
+          {q.incorrect_answers[0]}
+        </button>
+        <button onClick={(event) => props.handleClick(event)}>
+          {q.incorrect_answers[1]}
+        </button>
+        <button onClick={(event) => props.handleClick(event)}>
+          {q.incorrect_answers[2]}
+        </button>
         <hr />
       </div>
     );
   });
-  console.log(quizElements);
 
   return <>{quizElements}</>;
 }
